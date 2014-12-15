c7952f060d40ce7a683efce20becc291278246ef
j.pandeirada <joao.pandeirada@ua.pt>
2014-12-15 12:46:26 +0000

    joao

--- src/rmi_example.c
+++ src/rmi_example.c
@@ -19,23 +19,59 @@ int main(void)
 	{
 		printf("Press start to continue\n");
 		while(!startButton());
-		enableObstSens();
+		//enableObstSens();
 
 		
 		do
 		{
-			setVel2(50, 50);
-			waitTick40ms();						// Wait for next 40ms tick
+		//	setVel2(50, 50);
+			waitTick20ms();						// Wait for next 40ms tick
 			readAnalogSensors();				// Fill in "analogSensors" structure
 			groundSensor = readLineSensors(0);	// Read ground sensor
 			printf("Obst_left=%03d, Obst_center=%03d, Obst_right=%03d, Bat_voltage=%03d, Ground_sens=", analogSensors.obstSensLeft,
-							analogSensors.obstSensFront, analogSensors.obstSensRight, analogSensors.batteryVoltage);
+					analogSensors.obstSensFront, analogSensors.obstSensRight, analogSensors.batteryVoltage);
 
 			printInt(groundSensor, 2 | 5 << 16);	// System call
+			short sen_0 = (groundSensor  & 0x01);
+			short sen_1 = (groundSensor & 0x02)>>1;
+			short sen_2 = (groundSensor & 0x04)>>2;
+			short sen_3 = (groundSensor & 0x08)>>3;
+			short sen_4 = (groundSensor & 0x10)>>4;
+
+
+			/*if(!(sen_1|sen_2|sen_3)){
+				setVel2(-52,-49);
+			}else */
+			/*if(!(sen_1&sen_3)){
+				setVel2(0,0);
+			}else */if(!sen_2){
+
+				if(sen_1&sen_3){
+					setVel2(52,49);
+				}else if(!sen_3){
+					setVel2(35,73);
+				}else if(!sen_1){
+					setVel2(75,33);
+				}else{
+					setVel2(52,49);
+				}
+			} else if(!sen_3){
+				setVel2(20,97);
+			}else if(!sen_1){
+				setVel2(100,17);
+			}else{
+				setVel2(52,49);
+
+			}
+
+
+			printf("	%d %d %d %d %d ", sen_4, sen_3, sen_2, sen_1, sen_0);
+
+
 			printf("\n");
 		} while(!stopButton());
 		setVel2(0, 0);
-		disableObstSens();
+		//disableObstSens();
 		
 	}
 	return 0;
